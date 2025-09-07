7c3b60b4b39f3575c9ea6dba236a0b92135f8af9
AkaMICKY <akaastroyx@gmail.com>
2025-09-07 20:38:56 +0100

    Added Comments to refine Code

--- Age Calculator/Program.cs
+++ Age Calculator/Program.cs
@@ -13,8 +13,8 @@ namespace Age_Calculator
             Console.WriteLine("Hello, I Calculate How Old You Are, using Your BirthDay\nSO Lets get Started");
             Console.Write("When Is Your BirthDay(Year)? : ");
             string B_Day = Console.ReadLine();
-            int B_DAY = Convert.ToInt32(B_Day);
-            int Age = DateTime.Now.Year - B_DAY;
+            int B_DAY = Convert.ToInt32(B_Day); //Converts the Year(DOB) Input from String To Integer
+            int Age = DateTime.Now.Year - B_DAY; //Subtracts Users Input(B_DAY) from Current Year
             Console.WriteLine("You Are " + Age + " Years Old");
         }
     }
