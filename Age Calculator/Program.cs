c145d58cd8c9c151933b41eb1786eb8deb28e61d
AkaMICKY <akaastroyx@gmail.com>
2025-09-09 20:30:07 +0100

    Added failsafe to Prevent Negative Age

--- Age Calculator/Program.cs
+++ Age Calculator/Program.cs
@@ -14,32 +14,44 @@ namespace Age_Calculator
             Console.Write("When Is Your BirthDay(Year)? : ");
             string B_Day = Console.ReadLine();
             int B_DAY = Convert.ToInt32(B_Day); //Converts the Year(DOB) Input from String To Integer
-            int Age = DateTime.Now.Year - B_DAY; //Subtracts Users Input(B_DAY) from Current Year
-            Console.WriteLine("You Are " + Age + " Years Old");
-            if (Age < 5)
+            int Current_Year = DateTime.Now.Year;
+            if (Current_Year < B_DAY)
             {
-                Console.Write(" Hence You are a Baby/Toddler :)");
-            } else if (Age > 5 && Age < 12)
+                Console.WriteLine("Sorry Input A Correct Year, You Werent Born in the Future");
+            }else 
             {
-                Console.WriteLine(" Hence You are a Child");
-            } else if (Age > 12 && Age < 18)
+                int Age = DateTime.Now.Year - B_DAY; //Subtracts Users Input(B_DAY) from Current Year
+                Console.WriteLine("You Are " + Age + " Years Old");
+                 if (Age <= 5)
             {
-                Console.WriteLine(" Hence You are a Teenager ;)");
-            } else if (Age > 18 && Age < 27)
-            {
-                Console.WriteLine(" Hence You are a Young Adult :|");
+                    Console.Write(" Hence You are a Baby/Toddler :)");
+                } else if (Age > 5 && Age <= 12)
+                {
+                    Console.WriteLine(" Hence You are a Child");
+                }
+                else if (Age > 12 && Age <= 18)
+                {
+                    Console.WriteLine(" Hence You are a Teenager ;)");
+                }
+                else if (Age > 18 && Age <= 27)
+                {
+                    Console.WriteLine(" Hence You are a Young Adult :|");
+                }
+                else if (Age > 27 && Age <= 38)
+                {
+                    Console.WriteLine(" Hence You are Middle Age");
+                }
+                else if (Age > 38 && Age <= 56)
+                {
+                    Console.WriteLine(" Hence You are an Elder");
+                }
+                else
+                {
+                    Console.WriteLine(" Hence You Are an OLD Elder :(");
+                }
             }
-            else if (Age > 27 && Age < 38)
-            {
-                Console.WriteLine(" Hence You are Middle Age");
-            }
-            else if (Age > 38 && Age < 56)
-            {
-                Console.WriteLine(" Hence You are an Elder");
-            } else
-            {
-                Console.WriteLine(" Hence You Are an OLD Elder :(");
-            }
+            
+            
         }
     }
 }
